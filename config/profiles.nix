9b080a20193b922bc1956951546269e0cd4d1d1e
Chucklee1 <kermitthefrog@kakao.com>
2025-08-16 23:29:19 -0700

    fixed full and darwin not properly working by using real if statements

--- config/profiles.nix
+++ config/profiles.nix
@@ -1,21 +1,25 @@
-{
-  lib,
-  profile,
-  ...
-}:
-with lib; {
+{profile, ...}: {
   services.nixvim =
-    mkIf (profile == "core") {
-      formatting.enable = false;
-      telescope.enable = true;
-    }
-    // mkIf (profile == "full" || profile == "darwin") {
-      cmp.enable = true;
-      formatting.enable = true;
-      formatting.autoFormat = false;
-      latex.enable = true;
-      telescope.enable = true;
-      telescope.nerdIconLookup = true;
-    }
-    // mkIf (profile == "darwin") {latex.macSupport = true;};
+    (
+      if profile == "core"
+      then {
+        formatting.enable = false;
+        telescope.enable = true;
+      }
+      else if (profile == "full" || profile == "darwin")
+      then {
+        cmp.enable = true;
+        formatting.enable = true;
+        formatting.autoFormat = false;
+        latex.enable = true;
+        telescope.enable = true;
+        telescope.nerdIconLookup = true;
+      }
+      else {}
+    )
+    // (
+      if profile == "darwin"
+      then {latex.macSupport = true;}
+      else {}
+    );
 }
